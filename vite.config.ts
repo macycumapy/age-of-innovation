adc8b951a19f77cc1dbef9af46fc31f74d7a1860
macycumapy <kopperphild@gmail.com>
2026-08-31 11:59:01 +0500

    Добавлен игнор файлов в storage при билде

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import { bunny } from 'laravel-vite-plugin/fonts';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        watch: {
+            ignored: ['**/storage/**'],
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.ts'],
